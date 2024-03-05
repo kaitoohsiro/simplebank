b627f934a15456a90a6c647fc30eceaf41046b8c
kaitoohsiro <ooshirokaito@ooshirokaitonoMac-mini.local>
2024-03-05 16:35:08 +0900

    first commit

---
+++ util/random.go
@@ -0,0 +1,46 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	// rand seed非推奨により変更
+	seed := time.Now().UnixNano()
+	rand.New(rand.NewSource(seed))
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "CAD"}
+	n := len(currencies)
+
+	return currencies[rand.Intn(n)]
+}
